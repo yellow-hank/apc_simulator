0477c7812e433d3b03f822cdc3cc48e786cb54b2
yellowhank <shps97040608@gmail.com>
2022-06-02 21:27:50 +0800

    fix: docker image shouldn't be pushed first in ci

--- src/apcService/utilities/messageUtil.js
+++ src/apcService/utilities/messageUtil.js
@@ -9,13 +9,13 @@ const natsMessageHandler = (message) => {
 
   const msgObj = JSON.parse(message);
   if (msgObj.type === 'FACTOR_THICKNESS') {
-    // global.cache.set('FACTOR_THICKNESS', msgObj.factor);
+    global.cache.set('FACTOR_THICKNESS', msgObj.factor);
     const data = get();
     update({}, {"moisture": data.moisture, "thickness": msgObj.factor});
 
     logger.info(`receive thickness factor: ${msgObj.factor}`);
   } else if (msgObj.type === 'FACTOR_MOISTURE') {
-    // global.cache.set('FACTOR_MOISTURE', msgObj.factor);
+    global.cache.set('FACTOR_MOISTURE', msgObj.factor);
     const data = get();
     update({}, {"moisture": msgObj.factor, "thickness": data.thickness});
 
